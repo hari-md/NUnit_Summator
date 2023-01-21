fed9b3d21fcc03d3fbdac3abd95ba8ecd78a36d8
zdravko <zdravko@DESKTOP-K0CNT7D>
2023-01-21 15:20:17 +0200

    Add project files.

---
+++ Summator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1test) =>
+            //Console.WriteLine("Сумата е равна на: " + Summator.Sum(new int[] { 1, 2, 3 }));
+            //We call the class Summator to test the method Sum with new integer numbers (those in the array-{ 1, 2, 3 });
+
+
+            //2test) => 
+            //Summator.Test_SumTwoNumbers();
+            //Calls class Summator and tests 
+
+            //3test) =>
+            Summator.Test_SumEmptyArray();
+
+
+
+
+
+        }
+    }
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+        public static int Sum(int[] arr) //Sum is the Methed that we created. It can bbe tested with another program from the dev.
+        {
+            int sum = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        public static void Test_SumTwoNumbers()
+        {
+            if (Sum(new int[] { 1, 2 }) != 3)
+            {
+                throw new Exception("1+2 != 3");
+            }
+            else
+            {
+                Console.WriteLine("Test pass!");
+            }
+
+            // Calls method Sum and trows back an NEW exception if 1+2 !=3;
+        }
+
+        public static void Test_SumEmptyArray()
+        {
+            if (Sum(new int[] { }) != 0)
+            {
+                throw new Exception("sum [] != 0");
+            }
+        }
+    }
+}
